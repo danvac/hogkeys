ca601d421fe5a665d9eb92b54b6a9a9ceac3c4be
Will Hartsell <whartsell@gmail.com>
2012-10-27 13:36:34 -0500

    Changed from user.config to registry

--- src/HogKeys Devices/TestDriver.cs
+++ src/HogKeys Devices/TestDriver.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel;
-using net.willshouse.HogKeys.IO;
+//using net.willshouse.HogKeys.IO;
 using System.Net.Sockets;
 using System.Windows.Forms;
 using System.Collections.Concurrent;
