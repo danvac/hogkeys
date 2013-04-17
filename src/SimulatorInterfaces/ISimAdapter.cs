54382bf95bd37446145103e44867a204c9cb68cb
Will Hartsell <whartsell@gmail.com>
2013-04-17 11:35:18 -0500

    new model/layer to handle adapters to different simulators e.g x-plane,fsx,etc

---
+++ src/SimulatorInterfaces/ISimAdapter.cs
@@ -0,0 +1,21 @@
+﻿using net.willshouse.HogKeys.IO.Inputs;
+
+
+namespace net.willshouse.HogKeys.SimulatorAdapter
+{
+    public delegate void SimMessageEventHandler(object sender, SimMessageEventArgs e);
+
+    public interface ISimAdapter
+    {
+        int ListenerPort { get; set; }
+        int Port { get; set; }
+        string Host { get; set; }
+        bool Listening { get; }
+        
+        event SimMessageEventHandler MessageReceived;
+        void StartListening();
+        void StopListening();
+        void Send(Input input);
+
+    }
+}
